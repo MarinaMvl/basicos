48c411b731a57f98188207708a9f6964bdf97284
Maryna Ulsv <marynaulasava@gmail.com>
2019-12-09 13:18:31 +0100

    metodo para mostrar tipos numericos

--- src/basicosmarynaulasava/BasicosMarynaUlasava.java
+++ src/basicosmarynaulasava/BasicosMarynaUlasava.java
@@ -1,21 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package basicosmarynaulasava;
 
-/**
- *
- * @author Maryna Ulasava
- */
+
 public class BasicosMarynaUlasava {
 
-    /**
-     * @param args the command line arguments
-     */
+    /* Método para mostrar ejemplos de tipos numericos.*/
+     
+    public static void numericos() {
+
+        //Estos son los tipos básicos
+        byte elMasPeque;
+        short chiquitin;
+        int entero;
+        long duplicaEntero;
+        float grande;
+        double doble;
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        entero = 10;
+        doble = 5.6;
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        System.out.print("Valor entero ");
+        System.out.println(entero);
+        System.out.print("Valor doble ");
+        System.out.println(doble);
+    }
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        numericos(); //llamada al método
+        
     }
     
 }
