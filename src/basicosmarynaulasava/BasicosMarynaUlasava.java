d14281fb37e92662706a8ab098d6d059239bb324
Maryna Ulsv <marynaulasava@gmail.com>
2019-12-09 13:26:04 +0100

    metodo para mostrar ejemplo de clase String

--- src/basicosmarynaulasava/BasicosMarynaUlasava.java
+++ src/basicosmarynaulasava/BasicosMarynaUlasava.java
@@ -1,21 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package basicosmarynaulasava;
 
-/**
- *
- * @author Maryna Ulasava
- */
+
 public class BasicosMarynaUlasava {
 
-    /**
-     * @param args the command line arguments
-     */
+    /*metodo para mostrar ejemplos de clase String*/
+    public static void cadenas(){
+        //creación de la clase String
+        String s1;
+        String s2;
+        String s3;
+        
+        //ejemplos de asignación de valores a la clase String
+        s1="Un String en Java representa ";
+        s2= s1 + "una cadena de caracteres ";
+        s3= s2+ "no modificable.";
+        
+        //mostrar en pantalla el contenido del String
+        System.out.println(s3);
+        
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        cadenas(); //llamada al método
+        
+       
     }
     
 }
